9e38dfebdd0879d89b7bc28169daaba8245e5a59
eyedwalker <daw6921@g.harvard.edu>
2025-05-21 09:47:25 -0500

    Update Vercel configuration and app settings

--- app.py
+++ app.py
@@ -113,4 +113,4 @@ def webhook():
     return '', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
